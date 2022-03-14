7b3583775f52b3ce7cb869012852310db90d8698
AbdassalamAhmad <83673888+AbdassalamAhmad@users.noreply.github.com>
2022-03-14 13:31:53 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -9,17 +9,17 @@ path = "db"
 
 def training(path):
     images = os.listdir(path)
-    st.write(images)
+    
     encoded_trains =[]
     for image in images:
         train_img = face_recognition.load_image_file(f"db/{image}")
         train_img = cv2.cvtColor(train_img,cv2.COLOR_BGR2RGB)
         encoded_trains.append(face_recognition.face_encodings(train_img)[0])
-    return encoded_trains
+    return encoded_trains, images
 
 
-encoded_trains = training(path)
-
+encoded_trains, images = training(path)
+st.write(images)
 output_file = 'encoded_faces.pickle'
 with open(output_file, 'wb') as f_out:
     pickle.dump(encoded_trains, f_out)
